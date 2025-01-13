cc15ea85b46c239fb1453ef757dd5e97308dcf27
Zied-bousnina <zbousnina@yahoo.com>
2025-01-13 14:44:34 +0100

    I have changing the buttons connect with google and linkedin

--- components/partials/auth/social.jsx
+++ components/partials/auth/social.jsx
@@ -1,26 +1,32 @@
 const Social = () => {
   return (
-    <>
-      <ul className="flex">
+    <div className="flex justify-center gap-4 mt-4">
+      {/* LinkedIn Button */}
+      <a
+        href={`${process.env.NEXT_PUBLIC_API_URL}/api/users/linkedin`}
+        className="flex items-center justify-center w-12 h-12 rounded-full shadow-md hover:shadow-lg hover:bg-[#084a8a] transition-transform transform hover:-translate-y-1"
+        title="Connect with LinkedIn"
+      >
+        <img
+          src="/assets/images/icon/in.svg"
+          alt="LinkedIn"
+          className="w-8 h-8"
+        />
+      </a>
 
-        <li className="flex-1">
-          <a
-            href={`${process.env.NEXT_PUBLIC_API_URL}/api/users/linkedin`}
-            className="inline-flex h-10 w-10 bg-[#0A63BC] text-white text-2xl flex-col items-center justify-center rounded-full"
-          >
-            <img src="/assets/images/icon/in.svg" alt="" />
-          </a>
-        </li>
-        <li className="flex-1">
-          <a
-            href={`${process.env.NEXT_PUBLIC_API_URL}/api/users/google`}
-            className="inline-flex h-10 w-10 bg-[#EA4335] text-white text-2xl flex-col items-center justify-center rounded-full"
-          >
-            <img src="/assets/images/icon/gp.svg" alt="" />
-          </a>
-        </li>
-      </ul>
-    </>
+      {/* Google Button */}
+      <a
+        href={`${process.env.NEXT_PUBLIC_API_URL}/api/users/google`}
+        className="flex items-center justify-center w-12 h-12  rounded-full shadow-md hover:shadow-lg hover:bg-[#b43429] transition-transform transform hover:-translate-y-1"
+        title="Connect with Google"
+      >
+        <img
+          src="/assets/images/icon/gp.svg"
+          alt="Google"
+          className="w-8 h-8"
+        />
+      </a>
+    </div>
   );
 };
 
